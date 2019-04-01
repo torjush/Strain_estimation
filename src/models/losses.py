84bf7e77b3d614bab926f495ee69324c9dade49e
Torjus Haukom <torjus.haukom@icloud.com>
2019-04-01 13:58:27 +0200

    NCC without masking

--- src/models/losses.py
+++ src/models/losses.py
@@ -36,26 +36,19 @@ def nncc2d(fixed, warped):
     return -tf.reduce_mean(cc)
 
 
-def bendingPenalty(displacements, mask=None):
-    dx = tf.pad(displacements[:, :, 1:, :] - displacements[:, :, :-1, :],
-                [[0, 0], [0, 0], [0, 1], [0, 0]], mode='CONSTANT')
-    dy = tf.pad(displacements[:, 1:, :, :] - displacements[:, :-1, :, :],
-                [[0, 0], [0, 1], [0, 0], [0, 0]])
+def unmaskedNncc2d(fixed, warped):
+    fixed_mean = tf.reduce_mean(fixed, axis=[1, 2, 3], keep_dims=True)
+    warped_mean = tf.reduce_mean(warped, axis=[1, 2, 3], keep_dims=True)
 
-    dx_dx = tf.pad(dx[:, :, 1:, :] - dx[:, :, :-1, :],
-                   [[0, 0], [0, 0], [0, 1], [0, 0]], mode='CONSTANT')
-    dy_dy = tf.pad(dy[:, 1:, :, :] - dy[:, :-1, :, :],
-                   [[0, 0], [0, 1], [0, 0], [0, 0]], mode='CONSTANT')
+    N = tf.reduce_sum(tf.ones_like(fixed), axis=[1, 2, 3], keep_dims=True)
+    fixed_variance = tf.div(tf.reduce_sum(tf.square(fixed - fixed_mean),
+                                          axis=[1, 2, 3], keep_dims=True), N)
+    warped_variance = tf.div(tf.reduce_sum(tf.square(warped - warped_mean),
+                                           axis=[1, 2, 3], keep_dims=True), N)
 
-    dx_dy = tf.pad(dy[:, :, 1:, :] - dy[:, :, :-1, :],
-                   [[0, 0], [0, 0], [0, 1], [0, 0]], mode='CONSTANT')
+    numerator = (fixed - fixed_mean) * (warped - warped_mean)
+    denominator = tf.sqrt(fixed_variance * warped_variance)
 
-    dx_dx_2 = tf.reduce_sum(tf.square(dx_dx), axis=-1)
-    dy_dy_2 = tf.reduce_sum(tf.square(dy_dy), axis=-1)
-    dx_dy_2 = tf.reduce_sum(tf.square(dx_dy), axis=-1)
-
-    summed = dx_dx_2 + dy_dy_2 + 2 * dx_dy_2
-
-    per_img_pen = tf.reduce_sum(summed, axis=[1, 2])
-
-    return tf.reduce_mean(per_img_pen)
+    pixel_ncc = tf.div(numerator, denominator)
+    ncc = tf.reduce_mean(pixel_ncc, axis=[1, 2, 3])
+    return -tf.reduce_mean(ncc)
