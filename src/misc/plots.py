ee9c560d95e2efbbed901d5f44e56ede6c336a36
Torjus Haukom <torjus.haukom@icloud.com>
2019-04-10 09:40:44 +0200

    Modify grid plot

--- src/misc/plots.py
+++ src/misc/plots.py
@@ -1,10 +1,10 @@
 def plotGrid(ax, warped_grid, **kwargs):
-    gridx = warped_grid[7, 0, ::10, ::10]
-    gridy = warped_grid[7, 1, ::10, ::10]
+    gridx = warped_grid[0, ::10, ::10]
+    gridy = warped_grid[1, ::10, ::10]
 
-    for i in range(gridx.shape[0]):
+    for i in range(gridx.shape[0] - 1, 1, -1):
         ax.plot(gridx[i, :], gridy[i, :], **kwargs)
-    for i in range(gridx.shape[1]):
+    for i in range(gridx.shape[1] - 1, 1, -1):
         ax.plot(gridx[:, i], gridy[:, i], **kwargs)
 
 
